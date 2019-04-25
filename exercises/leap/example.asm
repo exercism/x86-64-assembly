46a4e68231cb1c613f552b10d96eeeda70a56eca
Johan Berg <47738833+bergjohan@users.noreply.github.com>
2019-04-26 00:24:57 +0200

    Add leap exercise

---
+++ exercises/leap/example.asm
@@ -0,0 +1,37 @@
+section .text
+global leap_year
+leap_year:
+    ; Check if year is divisible by 4
+    mov eax, edi
+    cdq
+    mov ecx, 4
+    div ecx
+    cmp edx, 0
+    jne .or
+
+    ; Check if year is divisible by 100
+    mov eax, edi
+    cdq
+    mov ecx, 100
+    div ecx
+    cmp edx, 0
+    jne .return_true
+
+.or:
+    ; Check if year is divisible by 400
+    mov eax, edi
+    cdq
+    mov ecx, 400
+    div ecx
+    cmp edx, 0
+    jne .return_false
+
+.return_true:
+    mov eax, 1
+    jmp .return
+
+.return_false:
+    mov eax, 0
+
+.return:
+    ret
---
+++ exercises/leap/leap_test.c
@@ -0,0 +1,51 @@
+#include <stdbool.h>
+
+#include "unity/unity.h"
+
+extern bool leap_year(int year);
+
+void setUp(void) {
+}
+
+void tearDown(void) {
+}
+
+void test_year_not_divisible_by_4_in_common_year(void) {
+    TEST_ASSERT_FALSE(leap_year(2015));
+}
+
+void test_year_divisible_by_2_not_divisible_by_4_in_common_year(void) {
+    TEST_IGNORE();
+    TEST_ASSERT_FALSE(leap_year(1970));
+}
+
+void test_year_divisible_by_4_not_divisible_by_100_in_leap_year(void) {
+    TEST_IGNORE();
+    TEST_ASSERT_TRUE(leap_year(1996));
+}
+
+void test_year_divisible_by_100_not_divisible_by_400_in_common_year(void) {
+    TEST_IGNORE();
+    TEST_ASSERT_FALSE(leap_year(2100));
+}
+
+void test_year_divisible_by_400_in_leap_year(void) {
+    TEST_IGNORE();
+    TEST_ASSERT_TRUE(leap_year(2000));
+}
+
+void year_divisible_by_200_not_divisible_by_400_in_common_year(void) {
+    TEST_IGNORE();
+    TEST_ASSERT_FALSE(leap_year(1800));
+}
+
+int main(void) {
+    UNITY_BEGIN();
+    RUN_TEST(test_year_not_divisible_by_4_in_common_year);
+    RUN_TEST(test_year_divisible_by_2_not_divisible_by_4_in_common_year);
+    RUN_TEST(test_year_divisible_by_4_not_divisible_by_100_in_leap_year);
+    RUN_TEST(test_year_divisible_by_100_not_divisible_by_400_in_common_year);
+    RUN_TEST(test_year_divisible_by_400_in_leap_year);
+    RUN_TEST(year_divisible_by_200_not_divisible_by_400_in_common_year);
+    return UNITY_END();
+}
