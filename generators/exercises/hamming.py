168f0e3d65e65a6503de38fbe8adcf5990d1ad49
Johan Berg <47738833+bergjohan@users.noreply.github.com>
2019-07-02 00:59:39 +0200

    hamming: Add generator

---
+++ generators/exercises/hamming.py
@@ -0,0 +1,12 @@
+FUNC_PROTO = """\
+#include "vendor/unity.h"
+
+extern int distance(const char *strand1, const char *strand2);
+"""
+
+def gen_func_body(prop, inp, expected):
+    strand1 = inp["strand1"]
+    strand2 = inp["strand2"]
+    if type(expected) is not int:
+        expected = -1
+    return f'TEST_ASSERT_EQUAL_INT({expected}, {prop}("{strand1}", "{strand2}"));\n'
