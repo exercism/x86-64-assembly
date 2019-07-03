962e9022068c6fdcd20c05c6637f538aad0b0f82
Johan Berg <47738833+bergjohan@users.noreply.github.com>
2019-07-04 01:09:50 +0200

    Add isogram exercise

---
+++ generators/exercises/isogram.py
@@ -0,0 +1,15 @@
+FUNC_PROTO = """\
+#include <stdbool.h>
+
+#include "vendor/unity.h"
+
+extern bool is_isogram(const char *phrase);
+"""
+
+def gen_func_body(prop, inp, expected):
+    phrase = inp["phrase"]
+    if expected:
+        s = f'TEST_ASSERT_TRUE({prop}("{phrase}"));\n'
+    else:
+        s = f'TEST_ASSERT_FALSE({prop}("{phrase}"));\n'
+    return s
