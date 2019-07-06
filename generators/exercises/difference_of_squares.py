6c20ad06a32b579b67d01c3569e92e64b2d97e86
Johan Berg <47738833+bergjohan@users.noreply.github.com>
2019-07-07 01:02:11 +0200

    Add difference-of-squares exercise

---
+++ generators/exercises/difference_of_squares.py
@@ -0,0 +1,11 @@
+FUNC_PROTO = """\
+#include "vendor/unity.h"
+
+extern int square_of_sum(int number);
+extern int sum_of_squares(int number);
+extern int difference_of_squares(int number);
+"""
+
+def gen_func_body(prop, inp, expected):
+    number = inp["number"]
+    return f"TEST_ASSERT_EQUAL_INT({expected}, {prop}({number}));\n"
