261174875cf8749b30a44d77eeef8f9a1c219379
Johan Berg <47738833+bergjohan@users.noreply.github.com>
2019-07-12 01:50:05 +0200

    hamming: Update comments

--- exercises/hamming/example.asm
+++ exercises/hamming/example.asm
@@ -4,38 +4,37 @@
 ; Parameters:
 ;   rdi - strand1
 ;   rsi - strand2
-;
 ; Returns:
-;   rax - distance
+;   rax - the difference between two DNA strands
 ;
 section .text
 global distance
 distance:
-    xor eax, eax          ; Set distance to 0
+    xor eax, eax        ; Initialize difference
 
-    cmp byte [rdi], 0     ; Found NUL in strand1?
-    je .loop_end          ; Yes => skip loop
-    cmp byte[rsi], 0      ; Found NUL in strand2?
-    je .loop_end          ; Yes => skip loop
+    cmp byte [rdi], 0   ; Check if strand1 is an empty string
+    je .loop_end        ; If empty, skip loop
+    cmp byte[rsi], 0    ; Check if strand2 is an empty string
+    je .loop_end        ; If empty, skip loop
 .loop_start:
-    mov cl, byte [rdi]    ; Fetch byte from strand1
-    cmp cl, byte [rsi]    ; Compare with strand2
-    je .skip              ; Equal => skip increment
-    inc eax               ; Increment distance
-.skip:
-    inc rdi
-    inc rsi
-    cmp byte [rdi], 0     ; Found NUL in strand1?
-    je .loop_end          ; Yes => exit loop
-    cmp byte [rsi], 0     ; Found NUL in strand2?
-    jne .loop_start       ; No => next iteration
+    mov cl, byte [rdi]  ; Read char from strand1
+    cmp cl, byte [rsi]  ; Compare with char from strand2
+    je .next            ; If equal, process next char
+    inc eax             ; Increment difference
+.next:
+    inc rdi             ; Advance strand1 to next char
+    inc rsi             ; Advance strand2 to next char
+    cmp byte [rdi], 0   ; See if we reached end of strand1
+    je .loop_end        ; If there are no more chars, exit loop
+    cmp byte [rsi], 0   ; See if we reached end of strand2
+    jne .loop_start     ; If chars remain, loop back
 
 .loop_end:
-    mov cl, byte [rdi]    ; Fetch byte from strand1
-    cmp cl, byte [rsi]    ; Compare with strand2
-    je .return            ; Equal => return distance
+    mov cl, byte [rdi]  ; Read char from strand1
+    cmp cl, byte [rsi]  ; Compare with char from strand2
+    je .return          ; If equal, return the difference
 
-    mov eax, -1           ; Set return value
+    mov eax, -1         ; Return -1 to indicate an error
 
 .return:
     ret
