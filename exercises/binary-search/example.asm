1389734ca5082e2506cea78a682efaf6db609880
Johan Berg <47738833+bergjohan@users.noreply.github.com>
2019-07-12 00:37:31 +0200

    binary-search: Update comments

--- exercises/binary-search/example.asm
+++ exercises/binary-search/example.asm
@@ -5,38 +5,36 @@
 ;   rdi - array
 ;   rsi - size
 ;   rdx - value
-;
 ; Returns:
-;   rax - the position of value
+;   rax - the position of value, or -1 if not found
 ;
 section .text
 global find
 find:
-    mov eax, -1                ; Set return value
-    test esi, esi              ; Is array NULL?
-    je .loop_end               ; Yes => skip loop
+    mov eax, -1              ; Initialize return value
+    test esi, esi            ; Check if array is empty
+    je .loop_end             ; If empty, return -1
 
-    xor ecx, ecx               ; Set left boundary to 0
-    lea r8d, [rsi - 1]         ; Set right boundary to n - 1
-    cmp ecx, r8d               ; Is left greater than right?
-    jg .loop_end               ; Yes => skip loop
+    xor ecx, ecx             ; Set left boundary to 0
+    lea r8d, [rsi - 1]       ; Set right boundary to size - 1
+    cmp ecx, r8d             ; Compare left and right boundaries
+    jg .loop_end             ; If left is greater than right, return -1
 .loop_start:
-    lea r9d, [ecx + r8d]       ; Set middle to left + right
-    sar r9d, 1                 ; Divide middle by 2
-    cmp [rdi + r9 * 4], edx    ; Is the middle array element less than value?
-    jl .less                   ; Yes => go to .less
-    cmp [rdi + r9 * 4], edx    ; Is the middle array element greater than value?
-    jg .greater                ; Yes => go to .greater
-    mov eax, r9d               ; Return middle
+    lea r9d, [rcx + r8]      ; Set the position of the middle element to left + right
+    sar r9d, 1               ; Divide by 2
+    cmp [rdi + r9 * 4], edx  ; Compare with target value
+    jl .less                 ; If less, go to .less
+    jg .greater              ; If greater, go to .greater
+    mov eax, r9d             ; Return the position of the middle element
     ret
 .less:
-    lea ecx, [r9 + 1]          ; Set left boundary to middle + 1
+    lea ecx, [r9 + 1]        ; Set left boundary to middle + 1
     jmp .next
 .greater:
-    lea r8d, [r9 - 1]          ; Set right boundary to middle - 1
+    lea r8d, [r9 - 1]        ; Set right boundary to middle - 1
 .next:
-    cmp ecx, r8d               ; Is left less or equal to right?
-    jle .loop_start            ; Yes => next iteration
+    cmp ecx, r8d             ; Compare left and right boundaries
+    jle .loop_start          ; If left is less or equal to right, loop back
 
 .loop_end:
    ret
