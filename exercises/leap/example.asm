d5d7494ad0afbe96d614fa8f742865f8c999067d
Johan Berg <47738833+bergjohan@users.noreply.github.com>
2019-07-14 01:09:25 +0200

    leap: Update comments

--- exercises/leap/example.asm
+++ exercises/leap/example.asm
@@ -3,38 +3,37 @@
 ;
 ; Parameters:
 ;   rdi - year
-;
 ; Returns:
-;   rax - 1 if leap year, else 0
+;   rax - true if leap year, else false
 ;
 section .text
 global leap_year
 leap_year:
-    mov eax, edi         ; Set dividend to year
-    cdq                  ; Sign-extend eax into edx
-    mov ecx, 4           ; Set divisor
+    mov eax, edi       ; Set dividend to year
+    cdq                ; Sign-extend eax into edx
+    mov ecx, 4         ; Set divisor
     idiv ecx
-    test edx, edx        ; Is remainder 0?
-    jne .return_false    ; No => return false
+    test edx, edx      ; Check if year is divisible by 4
+    jne .return_false  ; If not, return false
 
-    mov eax, edi         ; Set dividend to year
-    cdq                  ; Sign-extend eax into edx
-    mov ecx, 100         ; Set divisor
+    mov eax, edi       ; Set dividend to year
+    cdq                ; Sign-extend eax into edx
+    mov ecx, 100       ; Set divisor
     idiv ecx
-    test edx, edx        ; Is remainder 0?
-    jne .return_true     ; No => return true
+    test edx, edx      ; Check if year is divisible by 100
+    jne .return_true   ; If not, return true
 
-    mov eax, edi         ; Set dividend to year
-    cdq                  ; Sign-extend eax into edx
-    mov ecx, 400         ; Set divisor
+    mov eax, edi       ; Set dividend to year
+    cdq                ; Sign-extend eax into edx
+    mov ecx, 400       ; Set divisor
     idiv ecx
-    test edx, edx        ; Is remainder 0?
-    je .return_true      ; Yes => return true
+    test edx, edx      ; Check if year is divisible by 400
+    je .return_true    ; If so, return true
 
 .return_false:
-    xor eax, eax         ; Set return value
+    xor eax, eax       ; Return false
     ret
 
 .return_true:
-    mov eax, 1           ; Set return value
+    mov eax, 1         ; Return true
     ret
