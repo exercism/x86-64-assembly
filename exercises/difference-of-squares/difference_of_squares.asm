610ccd120c2570f2b52559b4e51fae6d52ab1e27
Johan Berg <47738833+bergjohan@users.noreply.github.com>
2019-08-05 00:30:27 +0200

    difference-of-squares: Add stub file

---
+++ exercises/difference-of-squares/difference_of_squares.asm
@@ -0,0 +1,15 @@
+section .text
+global square_of_sum
+square_of_sum:
+    ; Provide your implementation here
+    ret
+
+global sum_of_squares
+sum_of_squares:
+    ; Provide your implementation here
+    ret
+
+global difference_of_squares
+difference_of_squares:
+    ; Provide your implementation here
+    ret
