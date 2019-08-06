20790c94cdb4654649c6ae842613a4f35a7fad3e
Johan Berg <47738833+bergjohan@users.noreply.github.com>
2019-08-07 00:43:23 +0200

    isogram: Update parameter name

--- exercises/isogram/example.asm
+++ exercises/isogram/example.asm
@@ -2,7 +2,7 @@
 ; Determine if a word or phrase is an isogram.
 ;
 ; Parameters:
-;   rdi - phrase
+;   rdi - str
 ; Returns:
 ;   rax - true if isogram, else false
 ;
@@ -11,10 +11,10 @@ global is_isogram
 is_isogram:
     xor eax, eax           ; Initialize bitmap of used letters
 
-    cmp byte [rdi], 0      ; Check if phrase is an empty string
+    cmp byte [rdi], 0      ; Check if input string is empty
     je .loop_end           ; If empty, skip loop
 .loop_start:
-    movzx ecx, byte [rdi]  ; Read char from phrase
+    movzx ecx, byte [rdi]  ; Read char from input string
     or ecx, 32             ; If uppercase, convert to lowercase
     sub ecx, 'a'
     cmp ecx, 26            ; Check if alphabetic
@@ -22,8 +22,8 @@ is_isogram:
     bts eax, ecx           ; Mark letter as used in bitmap
     jc .loop_end           ; If letter has already been used, return false
 .next:
-    inc rdi                ; Advance phrase to next char
-    cmp byte [rdi], 0      ; See if we reached end of phrase
+    inc rdi                ; Advance input string to next char
+    cmp byte [rdi], 0      ; See if we reached the end
     jne .loop_start        ; If chars remain, loop back
 
 .loop_end:
