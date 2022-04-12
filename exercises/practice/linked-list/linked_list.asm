beedb0f59a7448edf005ad57b7dd5b18b63e0767
Sudhakar Verma <sudhakar.verma@crowdstrike.com>
2022-04-12 14:01:04 +0530

    exercises: add linked-list

---
+++ exercises/practice/linked-list/linked_list.asm
@@ -0,0 +1,40 @@
+section .text
+
+extern exit
+extern malloc
+extern free
+
+global list_create
+list_create:
+        ; Provide your implementation here
+        ret
+
+global list_count
+list_count:
+        ; Provide your implementation here
+        ret
+
+global list_push
+list_push:
+        ; Provide your implementation here
+        ret
+
+global list_pop
+list_pop:
+        ; Provide your implementation here
+        ret
+
+global list_unshift
+list_unshift:
+        ; Provide your implementation here
+        ret
+
+global list_shift
+list_shift:
+        ; Provide your implementation here
+        ret
+
+global list_destroy
+list_destroy:
+        ; Provide your implementation here
+        ret
