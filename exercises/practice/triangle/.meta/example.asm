c3172da1b65e8e410a16293dc48d6afc3c2e3600
Sudhakar Verma <sudhakar.verma@crowdstrike.com>
2022-06-20 10:18:44 +0530

    triangle: fix for #160 on macOS

--- exercises/practice/triangle/.meta/example.asm
+++ exercises/practice/triangle/.meta/example.asm
@@ -92,4 +92,6 @@ is_scalene:
         ret
 
 
+%ifidn __OUTPUT_FORMAT__,elf64
 section .note.GNU-stack noalloc noexec nowrite progbits
+%endif
--- exercises/practice/triangle/triangle.asm
+++ exercises/practice/triangle/triangle.asm
@@ -15,4 +15,6 @@ is_scalene:
     ; Provide your implementation here
     ret
 
+%ifidn __OUTPUT_FORMAT__,elf64
 section .note.GNU-stack noalloc noexec nowrite progbits
+%endif
