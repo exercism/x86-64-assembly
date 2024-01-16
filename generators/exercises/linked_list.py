9c7ad10fd6d81872e1ee8c3bcd50a084e7d73412
Sudhakar Verma <sudhakar.verma@canonical.com>
2024-01-16 18:32:29 +0530

    exercises: linked-list: update generator

--- generators/exercises/linked_list.py
+++ generators/exercises/linked_list.py
@@ -2,41 +2,31 @@ FUNC_PROTO = """\
 #include <stddef.h>
 #include "vendor/unity.h"
 
-typedef int ll_data_t;
-
-struct list_node {
-   struct list_node *prev, *next;
-   ll_data_t data;
-};
-
-struct list {
-   struct list_node *first, *last;
-};
-
-// constructs a new (empty) list
-extern struct list *list_create(void);
+// constructs a new (empty) list - called during setup
+// recommend : don't rely on `malloc`/`calloc`
+extern void list_create(void);
 
 // counts the items on a list
-extern size_t list_count(const struct list *list);
+extern size_t list_count();
 
 // inserts item at back of a list
-extern void list_push(struct list *list, ll_data_t item_data);
+extern void list_push(int item_data);
 
 // removes item from back of a list
-extern ll_data_t list_pop(struct list *list);
+extern int list_pop();
 
 // inserts item at front of a list
-extern void list_unshift(struct list *list, ll_data_t item_data);
+extern void list_unshift(int item_data);
 
 // removes item from front of a list
-extern ll_data_t list_shift(struct list *list);
+extern int list_shift();
 
 // deletes a node that holds the matching data
-extern void list_delete(struct list *list, ll_data_t data);
+extern void list_delete(int data);
 
-// destroys an entire list
-// list will be a dangling pointer after calling this method on it
-extern void list_destroy(struct list *list);
+// destroys an entire list - called during teardown
+// recommend : clear pointer and values for this run
+extern void list_destroy();
 
 """
 
@@ -44,10 +34,10 @@ def gen_func_body(prop, inp, expected):
     body = ""
     for op in inp.get("operations", []):
         if "expected" in op:
-            body += f'TEST_ASSERT_EQUAL({op["expected"]}, list_{op["operation"]}(list));\n'
+            body += f'TEST_ASSERT_EQUAL({op["expected"]}, list_{op["operation"]}());\n'
         else:
             if "value" in op:
-                body += f'list_{op["operation"]}(list, {op["value"]});\n'
+                body += f'list_{op["operation"]}({op["value"]});\n'
             else:
-                body += f'list_{op["operation"]}(list);\n'
+                body += f'list_{op["operation"]}();\n'
     return body
