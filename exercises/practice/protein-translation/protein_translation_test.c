e210067badaab847f4a0e709328e307d95b6b84b
Joel <D3usXMachina@users.noreply.github.com>
2024-02-15 00:19:35 +0100

    Add script to generate tests for protein-translation from canonical data

--- exercises/practice/protein-translation/protein_translation_test.c
+++ exercises/practice/protein-translation/protein_translation_test.c
@@ -1,20 +1,8 @@
-
-// Version: 1.0.0
+// Version: 0
 
 #include "vendor/unity.h"
 
 extern const char **proteins(const char *rna);
-
-void perform_test(const char *rna_strand, const char **expected, int expected_size) {
-    const char **names = proteins(rna_strand);
-    int size;
-    for (size = 0; names[size]; size++) {}
-
-    TEST_ASSERT_EQUAL_INT(expected_size, size);
-    if (expected_size > 0) {
-        TEST_ASSERT_EQUAL_STRING_ARRAY(expected, names, size);
-    }
-}
 
 void setUp(void) {
 }
@@ -22,163 +10,369 @@ void setUp(void) {
 void tearDown(void) {
 }
 
-void test_empty_sequence(void) {
-    perform_test("", NULL, 0);
+void test_empty_rna_sequence_results_in_no_proteins(void) {
+
+    const char **names = proteins("");
+
+    int size;
+    for (size = 0; names[size]; size++) {}
+    TEST_ASSERT_EQUAL_INT(0, size);
 }
 
-void test_methionine(void) {
+void test_methionine_rna_sequence(void) {
     TEST_IGNORE();
-    perform_test("AUG", (const char *[]){"Methionine"}, 1);
+
+    const char **names = proteins("AUG");
+
+    int size;
+    for (size = 0; names[size]; size++) {}
+    TEST_ASSERT_EQUAL_INT(1, size);
+
+    const char *expected[] = {"Methionine"};
+    TEST_ASSERT_EQUAL_STRING_ARRAY(expected, names, size);
 }
 
-void test_phenylalanine(void) {
+void test_phenylalanine_rna_sequence_1(void) {
     TEST_IGNORE();
-    perform_test("UUU", (const char *[]){"Phenylalanine"}, 1);
+
+    const char **names = proteins("UUU");
+
+    int size;
+    for (size = 0; names[size]; size++) {}
+    TEST_ASSERT_EQUAL_INT(1, size);
+
+    const char *expected[] = {"Phenylalanine"};
+    TEST_ASSERT_EQUAL_STRING_ARRAY(expected, names, size);
 }
 
-void test_phenylalanine_2(void) {
+void test_phenylalanine_rna_sequence_2(void) {
     TEST_IGNORE();
-    perform_test("UUC", (const char *[]){"Phenylalanine"}, 1);
+
+    const char **names = proteins("UUC");
+
+    int size;
+    for (size = 0; names[size]; size++) {}
+    TEST_ASSERT_EQUAL_INT(1, size);
+
+    const char *expected[] = {"Phenylalanine"};
+    TEST_ASSERT_EQUAL_STRING_ARRAY(expected, names, size);
 }
 
-void test_leucine(void) {
+void test_leucine_rna_sequence_1(void) {
     TEST_IGNORE();
-    perform_test("UUA", (const char *[]){"Leucine"}, 1);
+
+    const char **names = proteins("UUA");
+
+    int size;
+    for (size = 0; names[size]; size++) {}
+    TEST_ASSERT_EQUAL_INT(1, size);
+
+    const char *expected[] = {"Leucine"};
+    TEST_ASSERT_EQUAL_STRING_ARRAY(expected, names, size);
 }
 
-void test_leucine_2(void) {
+void test_leucine_rna_sequence_2(void) {
     TEST_IGNORE();
-    perform_test("UUG", (const char *[]){"Leucine"}, 1);
+
+    const char **names = proteins("UUG");
+
+    int size;
+    for (size = 0; names[size]; size++) {}
+    TEST_ASSERT_EQUAL_INT(1, size);
+
+    const char *expected[] = {"Leucine"};
+    TEST_ASSERT_EQUAL_STRING_ARRAY(expected, names, size);
 }
 
-void test_serine(void) {
+void test_serine_rna_sequence_1(void) {
     TEST_IGNORE();
-    perform_test("UCU", (const char *[]){"Serine"}, 1);
+
+    const char **names = proteins("UCU");
+
+    int size;
+    for (size = 0; names[size]; size++) {}
+    TEST_ASSERT_EQUAL_INT(1, size);
+
+    const char *expected[] = {"Serine"};
+    TEST_ASSERT_EQUAL_STRING_ARRAY(expected, names, size);
 }
 
-void test_serine_2(void) {
+void test_serine_rna_sequence_2(void) {
     TEST_IGNORE();
-    perform_test("UCC", (const char *[]){"Serine"}, 1);
+
+    const char **names = proteins("UCC");
+
+    int size;
+    for (size = 0; names[size]; size++) {}
+    TEST_ASSERT_EQUAL_INT(1, size);
+
+    const char *expected[] = {"Serine"};
+    TEST_ASSERT_EQUAL_STRING_ARRAY(expected, names, size);
 }
 
-void test_serine_3(void) {
+void test_serine_rna_sequence_3(void) {
     TEST_IGNORE();
-    perform_test("UCA", (const char *[]){"Serine"}, 1);
+
+    const char **names = proteins("UCA");
+
+    int size;
+    for (size = 0; names[size]; size++) {}
+    TEST_ASSERT_EQUAL_INT(1, size);
+
+    const char *expected[] = {"Serine"};
+    TEST_ASSERT_EQUAL_STRING_ARRAY(expected, names, size);
 }
 
-void test_serine_4(void) {
+void test_serine_rna_sequence_4(void) {
     TEST_IGNORE();
-    perform_test("UCG", (const char *[]){"Serine"}, 1);
+
+    const char **names = proteins("UCG");
+
+    int size;
+    for (size = 0; names[size]; size++) {}
+    TEST_ASSERT_EQUAL_INT(1, size);
+
+    const char *expected[] = {"Serine"};
+    TEST_ASSERT_EQUAL_STRING_ARRAY(expected, names, size);
 }
 
-void test_tyrosine(void) {
+void test_tyrosine_rna_sequence_1(void) {
     TEST_IGNORE();
-    perform_test("UAU", (const char *[]){"Tyrosine"}, 1);
+
+    const char **names = proteins("UAU");
+
+    int size;
+    for (size = 0; names[size]; size++) {}
+    TEST_ASSERT_EQUAL_INT(1, size);
+
+    const char *expected[] = {"Tyrosine"};
+    TEST_ASSERT_EQUAL_STRING_ARRAY(expected, names, size);
 }
 
-void test_tyrosine_2(void) {
+void test_tyrosine_rna_sequence_2(void) {
     TEST_IGNORE();
-    perform_test("UAC", (const char *[]){"Tyrosine"}, 1);
+
+    const char **names = proteins("UAC");
+
+    int size;
+    for (size = 0; names[size]; size++) {}
+    TEST_ASSERT_EQUAL_INT(1, size);
+
+    const char *expected[] = {"Tyrosine"};
+    TEST_ASSERT_EQUAL_STRING_ARRAY(expected, names, size);
 }
 
-void test_cysteine(void) {
+void test_cysteine_rna_sequence_1(void) {
     TEST_IGNORE();
-    perform_test("UGU", (const char *[]){"Cysteine"}, 1);
+
+    const char **names = proteins("UGU");
+
+    int size;
+    for (size = 0; names[size]; size++) {}
+    TEST_ASSERT_EQUAL_INT(1, size);
+
+    const char *expected[] = {"Cysteine"};
+    TEST_ASSERT_EQUAL_STRING_ARRAY(expected, names, size);
 }
 
-void test_cysteine_2(void) {
+void test_cysteine_rna_sequence_2(void) {
     TEST_IGNORE();
-   perform_test("UGC", (const char *[]){"Cysteine"}, 1);
+
+    const char **names = proteins("UGC");
+
+    int size;
+    for (size = 0; names[size]; size++) {}
+    TEST_ASSERT_EQUAL_INT(1, size);
+
+    const char *expected[] = {"Cysteine"};
+    TEST_ASSERT_EQUAL_STRING_ARRAY(expected, names, size);
 }
 
-void test_tryptophan(void) {
+void test_tryptophan_rna_sequence(void) {
     TEST_IGNORE();
-    perform_test("UGG", (const char *[]){"Tryptophan"}, 1);
+
+    const char **names = proteins("UGG");
+
+    int size;
+    for (size = 0; names[size]; size++) {}
+    TEST_ASSERT_EQUAL_INT(1, size);
+
+    const char *expected[] = {"Tryptophan"};
+    TEST_ASSERT_EQUAL_STRING_ARRAY(expected, names, size);
 }
 
-void test_stop_codon(void) {
+void test_stop_codon_rna_sequence_1(void) {
     TEST_IGNORE();
-    perform_test("UAA", NULL, 0);
+
+    const char **names = proteins("UAA");
+
+    int size;
+    for (size = 0; names[size]; size++) {}
+    TEST_ASSERT_EQUAL_INT(0, size);
 }
 
-void test_stop_codon_2(void) {
+void test_stop_codon_rna_sequence_2(void) {
     TEST_IGNORE();
-    perform_test("UAG", NULL, 0);
+
+    const char **names = proteins("UAG");
+
+    int size;
+    for (size = 0; names[size]; size++) {}
+    TEST_ASSERT_EQUAL_INT(0, size);
 }
 
-void test_stop_codon_3(void) {
+void test_stop_codon_rna_sequence_3(void) {
     TEST_IGNORE();
-    perform_test("UGA", NULL, 0);
+
+    const char **names = proteins("UGA");
+
+    int size;
+    for (size = 0; names[size]; size++) {}
+    TEST_ASSERT_EQUAL_INT(0, size);
 }
 
-void test_two_identical_codons(void) {
+void test_sequence_of_two_protein_codons_translates_into_proteins(void) {
     TEST_IGNORE();
-    perform_test("UUUUUU", (const char *[]){"Phenylalanine", "Phenylalanine"}, 2);
+
+    const char **names = proteins("UUUUUU");
+
+    int size;
+    for (size = 0; names[size]; size++) {}
+    TEST_ASSERT_EQUAL_INT(2, size);
+
+    const char *expected[] = {"Phenylalanine", "Phenylalanine"};
+    TEST_ASSERT_EQUAL_STRING_ARRAY(expected, names, size);
 }
 
-void test_two_different_codons(void) {
+void test_sequence_of_two_different_protein_codons_translates_into_proteins(void) {
     TEST_IGNORE();
-    perform_test("UUAUUG", (const char *[]){"Leucine", "Leucine"}, 2);
+
+    const char **names = proteins("UUAUUG");
+
+    int size;
+    for (size = 0; names[size]; size++) {}
+    TEST_ASSERT_EQUAL_INT(2, size);
+
+    const char *expected[] = {"Leucine", "Leucine"};
+    TEST_ASSERT_EQUAL_STRING_ARRAY(expected, names, size);
 }
 
-void test_three_codons(void) {
+void test_translate_rna_strand_into_correct_protein_list(void) {
     TEST_IGNORE();
-    perform_test("AUGUUUUGG", (const char *[]){"Methionine", "Phenylalanine", "Tryptophan"}, 3);
+
+    const char **names = proteins("AUGUUUUGG");
+
+    int size;
+    for (size = 0; names[size]; size++) {}
+    TEST_ASSERT_EQUAL_INT(3, size);
+
+    const char *expected[] = {"Methionine", "Phenylalanine", "Tryptophan"};
+    TEST_ASSERT_EQUAL_STRING_ARRAY(expected, names, size);
 }
 
-void test_stop_after_0_of_2(void) {
+void test_translation_stops_if_stop_codon_at_beginning_of_sequence(void) {
     TEST_IGNORE();
-    perform_test("UAGUGG", NULL, 0);
+
+    const char **names = proteins("UAGUGG");
+
+    int size;
+    for (size = 0; names[size]; size++) {}
+    TEST_ASSERT_EQUAL_INT(0, size);
 }
 
-void test_stop_after_1_of_2(void) {
+void test_translation_stops_if_stop_codon_at_end_of_twocodon_sequence(void) {
     TEST_IGNORE();
-    perform_test("UGGUAG", (const char *[]){"Tryptophan"}, 1);
+
+    const char **names = proteins("UGGUAG");
+
+    int size;
+    for (size = 0; names[size]; size++) {}
+    TEST_ASSERT_EQUAL_INT(1, size);
+
+    const char *expected[] = {"Tryptophan"};
+    TEST_ASSERT_EQUAL_STRING_ARRAY(expected, names, size);
 }
 
-void test_stop_after_2_of_3(void) {
+void test_translation_stops_if_stop_codon_at_end_of_threecodon_sequence(void) {
     TEST_IGNORE();
-    perform_test("AUGUUUUAA", (const char *[]){"Methionine", "Phenylalanine"}, 2);
+
+    const char **names = proteins("AUGUUUUAA");
+
+    int size;
+    for (size = 0; names[size]; size++) {}
+    TEST_ASSERT_EQUAL_INT(2, size);
+
+    const char *expected[] = {"Methionine", "Phenylalanine"};
+    TEST_ASSERT_EQUAL_STRING_ARRAY(expected, names, size);
 }
 
-void test_stop_after_1_of_3(void) {
+void test_translation_stops_if_stop_codon_in_middle_of_threecodon_sequence(void) {
     TEST_IGNORE();
-    perform_test("UGGUAGUGG", (const char *[]){"Tryptophan"}, 1);
+
+    const char **names = proteins("UGGUAGUGG");
+
+    int size;
+    for (size = 0; names[size]; size++) {}
+    TEST_ASSERT_EQUAL_INT(1, size);
+
+    const char *expected[] = {"Tryptophan"};
+    TEST_ASSERT_EQUAL_STRING_ARRAY(expected, names, size);
 }
 
-void test_stop_after_3_of_6(void) {
+void test_translation_stops_if_stop_codon_in_middle_of_sixcodon_sequence(void) {
     TEST_IGNORE();
-    perform_test("UGGUGUUAUUAAUGGUUU", (const char *[]){"Tryptophan", "Cysteine", "Tyrosine"}, 3);
+
+    const char **names = proteins("UGGUGUUAUUAAUGGUUU");
+
+    int size;
+    for (size = 0; names[size]; size++) {}
+    TEST_ASSERT_EQUAL_INT(3, size);
+
+    const char *expected[] = {"Tryptophan", "Cysteine", "Tyrosine"};
+    TEST_ASSERT_EQUAL_STRING_ARRAY(expected, names, size);
+}
+
+void test_incomplete_rna_sequence_can_translate_if_valid_until_a_stop_codon(void) {
+    TEST_IGNORE();
+
+    const char **names = proteins("UUCUUCUAAUGGU");
+
+    int size;
+    for (size = 0; names[size]; size++) {}
+    TEST_ASSERT_EQUAL_INT(2, size);
+
+    const char *expected[] = {"Phenylalanine", "Phenylalanine"};
+    TEST_ASSERT_EQUAL_STRING_ARRAY(expected, names, size);
 }
 
 int main(void) {
     UNITY_BEGIN();
-    RUN_TEST(test_empty_sequence);
-    RUN_TEST(test_empty_sequence);
-    RUN_TEST(test_methionine);
-    RUN_TEST(test_phenylalanine);
-    RUN_TEST(test_phenylalanine_2);
-    RUN_TEST(test_leucine);
-    RUN_TEST(test_leucine_2);
-    RUN_TEST(test_serine);
-    RUN_TEST(test_serine_2);
-    RUN_TEST(test_serine_3);
-    RUN_TEST(test_serine_4);
-    RUN_TEST(test_tyrosine);
-    RUN_TEST(test_tyrosine_2);
-    RUN_TEST(test_cysteine);
-    RUN_TEST(test_cysteine_2);
-    RUN_TEST(test_tryptophan);
-    RUN_TEST(test_stop_codon);
-    RUN_TEST(test_stop_codon_2);
-    RUN_TEST(test_stop_codon_3);
-    RUN_TEST(test_two_identical_codons);
-    RUN_TEST(test_two_different_codons);
-    RUN_TEST(test_three_codons);
-    RUN_TEST(test_stop_after_0_of_2);
-    RUN_TEST(test_stop_after_1_of_2);
-    RUN_TEST(test_stop_after_2_of_3);
-    RUN_TEST(test_stop_after_1_of_3);
-    RUN_TEST(test_stop_after_3_of_6);
+    RUN_TEST(test_empty_rna_sequence_results_in_no_proteins);
+    RUN_TEST(test_methionine_rna_sequence);
+    RUN_TEST(test_phenylalanine_rna_sequence_1);
+    RUN_TEST(test_phenylalanine_rna_sequence_2);
+    RUN_TEST(test_leucine_rna_sequence_1);
+    RUN_TEST(test_leucine_rna_sequence_2);
+    RUN_TEST(test_serine_rna_sequence_1);
+    RUN_TEST(test_serine_rna_sequence_2);
+    RUN_TEST(test_serine_rna_sequence_3);
+    RUN_TEST(test_serine_rna_sequence_4);
+    RUN_TEST(test_tyrosine_rna_sequence_1);
+    RUN_TEST(test_tyrosine_rna_sequence_2);
+    RUN_TEST(test_cysteine_rna_sequence_1);
+    RUN_TEST(test_cysteine_rna_sequence_2);
+    RUN_TEST(test_tryptophan_rna_sequence);
+    RUN_TEST(test_stop_codon_rna_sequence_1);
+    RUN_TEST(test_stop_codon_rna_sequence_2);
+    RUN_TEST(test_stop_codon_rna_sequence_3);
+    RUN_TEST(test_sequence_of_two_protein_codons_translates_into_proteins);
+    RUN_TEST(test_sequence_of_two_different_protein_codons_translates_into_proteins);
+    RUN_TEST(test_translate_rna_strand_into_correct_protein_list);
+    RUN_TEST(test_translation_stops_if_stop_codon_at_beginning_of_sequence);
+    RUN_TEST(test_translation_stops_if_stop_codon_at_end_of_twocodon_sequence);
+    RUN_TEST(test_translation_stops_if_stop_codon_at_end_of_threecodon_sequence);
+    RUN_TEST(test_translation_stops_if_stop_codon_in_middle_of_threecodon_sequence);
+    RUN_TEST(test_translation_stops_if_stop_codon_in_middle_of_sixcodon_sequence);
+    RUN_TEST(test_incomplete_rna_sequence_can_translate_if_valid_until_a_stop_codon);
     return UNITY_END();
 }
