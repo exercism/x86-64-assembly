56e25f6a10125da004daf5c054bc3f305ef67ab3
keiravillekode <keiravillekode@gmail.com>
2024-12-30 19:12:48 +1100

    Add book-store exercise (#245)

---
+++ generators/exercises/book_store.py
@@ -0,0 +1,21 @@
+FUNC_PROTO = """\
+#include "stdint.h"
+#include "vendor/unity.h"
+
+#define ARRAY_SIZE(x) (sizeof(x) / sizeof((x)[0]))
+
+extern int64_t total(int64_t count, const int16_t *basket);
+"""
+
+def gen_func_body(prop, inp, expected):
+    str_list = []
+    if (len(inp["basket"]) > 0):
+        basket = str(inp["basket"]).replace("[", "{").replace("]", "}")
+        str_list.append(f"int16_t basket[] = {basket};\n")
+        count = "ARRAY_SIZE(basket)"
+    else:
+        str_list.append("const int16_t* basket = NULL;\n")
+        count = "0"
+
+    str_list.append(f"TEST_ASSERT_EQUAL_INT({expected}, {prop}({count}, basket));\n")
+    return "".join(str_list)
