63db0dbb068641c9c53fc077b328df769e164628
oxe-i <143895606+oxe-i@users.noreply.github.com>
2025-08-13 19:49:47 -0300

    add micro-blog exercise (#294)
    * change generator to the one used in ARM assembly track, to handle extra cases.
    
    * add clang-format support
    
    by: oxe-i <oxe-b@gmail.com>

---
+++ exercises/practice/micro-blog/.meta/example.asm
@@ -0,0 +1,55 @@
+section .text
+global truncate
+truncate:
+    ; rdi - output char buffer
+    ; rsi - input string
+    ; output buffer should be modified in place
+    ; return is void
+
+    ; UTF-8 Unicode characters may have 1, 2, 3 or 4 bytes
+    ; 1 byte UTF-8 are ordinary ASCII chars and have the leading bit off
+    ; 2 bytes UTF-8 have the first byte with two leading bits on and the third off
+    ; 3 bytes UTF-8 have the first byte with three leading bits on and the fourth off
+    ; and 4 bytes UTF-8 have the first byte with four leading bits on and the fifth off
+    ;
+    ; In every case where there are more than 1 byte, 
+    ; the non-leading bytes have the leading bit on and the second off
+    ;
+    ; This is a straightforward implementation which moves all the necessary bytes in the same iteration
+    ; after checking for the bits in the leading byte
+    ; it'd probably be safer, in case of invalid chars, to process one byte at a time and stop when reaching NULL
+    
+    mov rcx, 5
+main_loop:
+    mov r8b, byte [rsi]
+
+    bt r8, 7
+    jnc ASCII ; leading bit off
+
+    bt r8, 5
+    jnc two_byte ; just two leading bits on
+
+    bt r8, 4
+    jnc three_byte ; just three leading bits on
+
+    ; all four leading bits on
+
+    movsb
+three_byte:
+    movsb
+two_byte:
+    movsb
+ASCII:
+    movsb
+
+    cmp r8b, 0
+    loopne main_loop ; loops 5 times or until leading byte is NULL
+end:
+    mov al, 0
+    stosb ; adds NULL to the end, in case it wasn't added in the loop
+    
+    ret
+
+%ifidn __OUTPUT_FORMAT__,elf64
+section .note.GNU-stack noalloc noexec nowrite progbits
+%endif
---
+++ generators/exercises/micro_blog.py
@@ -0,0 +1,26 @@
+FUNC_PROTO = """\
+#include "vendor/unity.h"
+
+#define BUFFER_SIZE 40
+
+extern void truncate(char *buffer, const char *phrase);
+"""
+
+def extra_cases():
+    return [
+        {
+            "description": "ideograms",
+            "property": "truncate",
+            "input": {"phrase": "二兎を追う者は一兎をも得ず"},
+            "expected": "二兎を追う",
+        }
+    ]
+
+
+def gen_func_body(prop, inp, expected):
+    phrase = inp["phrase"]
+    str_list = []
+    str_list.append("char buffer[BUFFER_SIZE];\n\n")
+    str_list.append(f'{prop}(buffer, "{phrase}");\n')
+    str_list.append(f'TEST_ASSERT_EQUAL_STRING("{expected}", buffer);\n')
+    return "".join(str_list)
