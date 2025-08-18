7c9f4fe3e7d8c4be332a65222030a4383f7ec260
oxe-i <oxe-b@gmail.com>
2025-08-18 13:21:46 -0300

    pre-commit adjustments

--- generators/exercises/matrix.py
+++ generators/exercises/matrix.py
@@ -11,17 +11,22 @@ extern size_t row(int32_t *buffer, const char *input, size_t index);
 extern size_t column(int32_t *buffer, const char *input, size_t index);
 """
 
+
 def array_literal(numbers):
     return str(numbers).replace("[", "{").replace("]", "}")
+
 
 def gen_func_body(prop, inp, expected):
     input_string = str(inp["string"]).replace("\n", "\\n")
     index = inp["index"]
     str_list = []
     str_list.append("int32_t buffer[BUFFER_SIZE];\n\n")
-    str_list.append(f'const int32_t expected[] = {array_literal(expected)};\n')
-    str_list.append(f'const size_t length = {prop}(buffer, "{input_string}", {index});\n')
-    str_list.append(f'TEST_ASSERT_EQUAL_UINT64(ARRAY_SIZE(expected), length);\n')
-    str_list.append(f'TEST_ASSERT_EQUAL_INT32_ARRAY(expected, buffer, ARRAY_SIZE(expected));\n')
+    str_list.append(f"const int32_t expected[] = {array_literal(expected)};\n")
+    str_list.append(
+        f'const size_t length = {prop}(buffer, "{input_string}", {index});\n'
+    )
+    str_list.append("TEST_ASSERT_EQUAL_UINT64(ARRAY_SIZE(expected), length);\n")
+    str_list.append(
+        "TEST_ASSERT_EQUAL_INT32_ARRAY(expected, buffer, ARRAY_SIZE(expected));\n"
+    )
     return "".join(str_list)
-
