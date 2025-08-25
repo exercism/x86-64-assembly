8307b9281faf3d4fcd9a7272473d116f52f32502
oxe-i <oxe-b@gmail.com>
2025-08-24 22:11:22 -0300

    add struct name in clock exercise

--- generators/exercises/clock.py
+++ generators/exercises/clock.py
@@ -8,12 +8,12 @@ FUNC_PROTO = """\
 typedef struct {
     uint8_t hour;
     uint8_t minute;
-} Clock_t;
+} clock_time_t;
 
-extern Clock_t create(int64_t hour, int64_t minute);
-extern Clock_t add_minutes(int64_t hour, int64_t minute, int64_t value);
-extern Clock_t subtract_minutes(int64_t hour, int64_t minute, int64_t value);
-extern bool equal(Clock_t clock1, Clock_t clock2);
+extern clock_time_t create(int64_t hour, int64_t minute);
+extern clock_time_t add_minutes(int64_t hour, int64_t minute, int64_t value);
+extern clock_time_t subtract_minutes(int64_t hour, int64_t minute, int64_t value);
+extern bool equal(clock_time_t clock1, clock_time_t clock2);
 """
 
 
@@ -27,32 +27,36 @@ def gen_func_body(prop, inp, expected):
         expected = str(expected).lower()
         clock1 = list(inp["clock1"].values())
         clock2 = list(inp["clock2"].values())
-        str_list.append(f"const Clock_t clock1 = create({clock1[0]}, {clock1[1]});\n")
-        str_list.append(f"const Clock_t clock2 = create({clock2[0]}, {clock2[1]});\n")
+        str_list.append(
+            f"const clock_time_t clock1 = create({clock1[0]}, {clock1[1]});\n"
+        )
+        str_list.append(
+            f"const clock_time_t clock2 = create({clock2[0]}, {clock2[1]});\n"
+        )
         str_list.append(f"TEST_ASSERT_EQUAL({expected}, {prop}(clock1, clock2));\n")
     elif prop == "create":
         hour, minute = expected.split(":")
         str_list.append(
-            "const Clock_t expected = "
+            "const clock_time_t expected = "
             + "{"
             + f"{re.sub(r'0(?=\d)', '', hour)}, {re.sub(r'0(?=\d)', '', minute)}"
             + "};\n"
         )
         str_list.append(
-            f"const Clock_t clock = {prop}({inp['hour']}, {inp['minute']});\n"
+            f"const clock_time_t clock = {prop}({inp['hour']}, {inp['minute']});\n"
         )
         str_list.append("TEST_ASSERT_EQUAL_UINT8(expected.hour, clock.hour);\n")
         str_list.append("TEST_ASSERT_EQUAL_UINT8(expected.minute, clock.minute);\n")
     else:
         hour, minute = expected.split(":")
         str_list.append(
-            "const Clock_t expected = "
+            "const clock_time_t expected = "
             + "{"
             + f"{re.sub(r'0(?=\d)', '', hour)}, {re.sub(r'0(?=\d)', '', minute)}"
             + "};\n"
         )
         str_list.append(
-            f"const Clock_t clock = {prop}_minutes({inp['hour']}, {inp['minute']}, {inp['value']});\n"
+            f"const clock_time_t clock = {prop}_minutes({inp['hour']}, {inp['minute']}, {inp['value']});\n"
         )
         str_list.append("TEST_ASSERT_EQUAL_UINT8(expected.hour, clock.hour);\n")
         str_list.append("TEST_ASSERT_EQUAL_UINT8(expected.minute, clock.minute);\n")
