2b61e8da759dd40c603d7309e54bb18e22c299e8
Eric Willigers <keiravillekode@gmail.com>
2025-09-08 11:25:51 +1000

    regenerate hamming tests

--- exercises/practice/hamming/hamming_test.c
+++ exercises/practice/hamming/hamming_test.c
@@ -1,5 +1,3 @@
-// Version: 2.3.0
-
 #include "vendor/unity.h"
 
 extern int distance(const char *strand1, const char *strand2);
@@ -49,7 +47,17 @@ void test_disallow_left_empty_strand(void) {
     TEST_ASSERT_EQUAL_INT(-1, distance("", "G"));
 }
 
+void test_disallow_empty_first_strand(void) {
+    TEST_IGNORE();
+    TEST_ASSERT_EQUAL_INT(-1, distance("", "G"));
+}
+
 void test_disallow_right_empty_strand(void) {
+    TEST_IGNORE();
+    TEST_ASSERT_EQUAL_INT(-1, distance("G", ""));
+}
+
+void test_disallow_empty_second_strand(void) {
     TEST_IGNORE();
     TEST_ASSERT_EQUAL_INT(-1, distance("G", ""));
 }
@@ -64,6 +72,8 @@ int main(void) {
     RUN_TEST(test_disallow_first_strand_longer);
     RUN_TEST(test_disallow_second_strand_longer);
     RUN_TEST(test_disallow_left_empty_strand);
+    RUN_TEST(test_disallow_empty_first_strand);
     RUN_TEST(test_disallow_right_empty_strand);
+    RUN_TEST(test_disallow_empty_second_strand);
     return UNITY_END();
 }
