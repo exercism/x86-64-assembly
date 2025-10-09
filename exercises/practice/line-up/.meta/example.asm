a314f2987985a882bfbc9a3154e6246482fac081
oxe-i <oxe-b@gmail.com>
2025-10-08 21:31:37 -0300

    add more comments explaining the code

--- exercises/practice/line-up/.meta/example.asm
+++ exercises/practice/line-up/.meta/example.asm
@@ -21,38 +21,38 @@ format:
     mov rcx, qword [mid_text_sz]
     rep movsb ; add middle text
     mov ax, dx
-    mov rsi, rsp
+    mov rsi, rsp ; rsi now points to the top of the stack
     mov r10, 10
 .stringify:
     cwd
     div r10w
-    add dl, '0'
-    dec rsi
-    mov byte [rsi], dl
+    add dl, '0' ; converts digit to its char equivalent
+    dec rsi ; allocates 1 byte in the stack
+    mov byte [rsi], dl ; saves char digit in the stack
     test ax, ax
     jnz .stringify ; stringify number, saving chars in the stack, in reversed order
     mov rcx, rsp
-    sub rcx, rsi
+    sub rcx, rsi ; rcx is now the size of the stringified number (rsp - rsi)
     rep movsb ; add stringified number
-    lea rsi, [th]
+    lea rsi, [th] ; default ordinal suffix is "th"
     cmp byte [rsp - 2], '1'
-    setne r8b
+    setne r8b ; true if number does not end in "1x", where x is a digit ['0'..'9']
     cmp byte [rsp - 1], '1'
-    sete r9b
+    sete r9b ; true if number ends in '1'
     lea rdx, [first]
     test r8b, r9b
     cmovnz rsi, rdx ; if second last digit is not '1' and last digit is '1', suffix is "st"
     cmp byte [rsp - 1], '2'
-    sete r9b
+    sete r9b ; true if number ends in '2'
     lea rdx, [second]
     test r8b, r9b
     cmovnz rsi, rdx ; if second last digit is not '1' and last digit is '2', suffix is "nd"
     cmp byte [rsp - 1], '3'
-    sete r9b
+    sete r9b ; true if number ends in '3'
     lea rdx, [third]
     test r8b, r9b
     cmovnz rsi, rdx ; if second last digit is not '1' and last digit is '3', suffix is "rd"
-    mov rcx, 2
+    mov rcx, 2 ; all ordinal suffixs have size 2
     rep movsb ; add ordinal suffix
     lea rsi, [end_text]
     mov rcx, qword [end_text_sz]
