f36401dcf1992065fa50ea16b3f16442fe3f1217
oxe-i <oxe.github@gmail.com>
2026-01-08 16:38:42 -0300

    associate space age practice exercise with floats and arrays, adjust prerequisite for high-scores (#413)

--- exercises/practice/space-age/space_age.asm
+++ exercises/practice/space-age/space_age.asm
@@ -2,6 +2,18 @@ section .text
 global age
 age:
     ; Provide your implementation here
+    ; The function has type signature float age(enum planet planet, int seconds)
+    ; The return value is of type float, which is a 32-bit floating-point number
+    ; The first argument is of type enum planet, which is a 32-bit signed integer that represents a planet:
+    ; 0 -> Mercury
+    ; 1 -> Venus
+    ; 2 -> Earth
+    ; 3 -> Mars
+    ; 4 -> Jupiter
+    ; 5 -> Saturn
+    ; 6 -> Uranus
+    ; 7 -> Neptune
+    ; The second argument is of type int, which is a 32-bit signed integer
     ret
 
 %ifidn __OUTPUT_FORMAT__,elf64
--- exercises/practice/two-fer/two_fer.asm
+++ exercises/practice/two-fer/two_fer.asm
@@ -4,8 +4,10 @@ two_fer:
     ; Provide your implementation here
     ; The function has type signature void two_fer(const char *name, char *buffer)
     ; It has no return value
-    ; The first argument is of type const char*, which is the address of a read-only NUL-terminated sequence of bytes stored in memory
+    ; The first argument is of type const char*, which is the address of a read-only NUL-terminated sequence of bytes stored in memory that represents the name of a person
+    ; If no name is passed, this address has a value of 0
     ; The second argument is of type char*, which is the address of a writable sequence of bytes stored in memory
+    ; The resulting string should be NUL-terminated and stored at the location pointed to by the address provided in the second argument.
     ret
 
 %ifidn __OUTPUT_FORMAT__,elf64
