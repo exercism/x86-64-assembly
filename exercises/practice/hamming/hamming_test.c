ffccbfc520a54281b73c86732998e7d58c67e3c5
oxe-i <oxe-b@gmail.com>
2026-05-02 19:46:00 -0300

    alignas, assert string length < 16

--- exercises/practice/hamming/hamming_test.c
+++ exercises/practice/hamming/hamming_test.c
@@ -1,5 +1,7 @@
 #include "vendor/unity.h"
 
+#include <stdalign.h>
+
 #define BUFFER_SIZE 16
 
 extern int distance(const char strand1[], const char strand2[]);
@@ -11,78 +13,78 @@ void tearDown(void) {
 }
 
 void test_empty_strands(void) {
-    const char strand1[BUFFER_SIZE] = "";
-    const char strand2[BUFFER_SIZE] = "";
+    alignas(16) const char strand1[BUFFER_SIZE] = "";
+    alignas(16) const char strand2[BUFFER_SIZE] = "";
     TEST_ASSERT_EQUAL_INT(0, distance(strand1, strand2));
 }
 
 void test_single_letter_identical_strands(void) {
     TEST_IGNORE();
-    const char strand1[BUFFER_SIZE] = "A";
-    const char strand2[BUFFER_SIZE] = "A";
+    alignas(16) const char strand1[BUFFER_SIZE] = "A";
+    alignas(16) const char strand2[BUFFER_SIZE] = "A";
     TEST_ASSERT_EQUAL_INT(0, distance(strand1, strand2));
 }
 
 void test_single_letter_different_strands(void) {
     TEST_IGNORE();
-    const char strand1[BUFFER_SIZE] = "G";
-    const char strand2[BUFFER_SIZE] = "T";
+    alignas(16) const char strand1[BUFFER_SIZE] = "G";
+    alignas(16) const char strand2[BUFFER_SIZE] = "T";
     TEST_ASSERT_EQUAL_INT(1, distance(strand1, strand2));
 }
 
 void test_long_identical_strands(void) {
     TEST_IGNORE();
-    const char strand1[BUFFER_SIZE] = "GGACTGAAATCTG";
-    const char strand2[BUFFER_SIZE] = "GGACTGAAATCTG";
+    alignas(16) const char strand1[BUFFER_SIZE] = "GGACTGAAATCTG";
+    alignas(16) const char strand2[BUFFER_SIZE] = "GGACTGAAATCTG";
     TEST_ASSERT_EQUAL_INT(0, distance(strand1, strand2));
 }
 
 void test_long_different_strands(void) {
     TEST_IGNORE();
-    const char strand1[BUFFER_SIZE] = "GGACGGATTCTG";
-    const char strand2[BUFFER_SIZE] = "AGGACGGATTCT";
+    alignas(16) const char strand1[BUFFER_SIZE] = "GGACGGATTCTG";
+    alignas(16) const char strand2[BUFFER_SIZE] = "AGGACGGATTCT";
     TEST_ASSERT_EQUAL_INT(9, distance(strand1, strand2));
 }
 
 void test_disallow_first_strand_longer(void) {
     TEST_IGNORE();
-    const char strand1[BUFFER_SIZE] = "AATG";
-    const char strand2[BUFFER_SIZE] = "AAA";
+    alignas(16) const char strand1[BUFFER_SIZE] = "AATG";
+    alignas(16) const char strand2[BUFFER_SIZE] = "AAA";
     TEST_ASSERT_EQUAL_INT(-1, distance(strand1, strand2));
 }
 
 void test_disallow_second_strand_longer(void) {
     TEST_IGNORE();
-    const char strand1[BUFFER_SIZE] = "ATA";
-    const char strand2[BUFFER_SIZE] = "AGTG";
+    alignas(16) const char strand1[BUFFER_SIZE] = "ATA";
+    alignas(16) const char strand2[BUFFER_SIZE] = "AGTG";
     TEST_ASSERT_EQUAL_INT(-1, distance(strand1, strand2));
 }
 
 void test_disallow_left_empty_strand(void) {
     TEST_IGNORE();
-    const char strand1[BUFFER_SIZE] = "";
-    const char strand2[BUFFER_SIZE] = "G";
+    alignas(16) const char strand1[BUFFER_SIZE] = "";
+    alignas(16) const char strand2[BUFFER_SIZE] = "G";
     TEST_ASSERT_EQUAL_INT(-1, distance(strand1, strand2));
 }
 
 void test_disallow_empty_first_strand(void) {
     TEST_IGNORE();
-    const char strand1[BUFFER_SIZE] = "";
-    const char strand2[BUFFER_SIZE] = "G";
+    alignas(16) const char strand1[BUFFER_SIZE] = "";
+    alignas(16) const char strand2[BUFFER_SIZE] = "G";
     TEST_ASSERT_EQUAL_INT(-1, distance(strand1, strand2));
 }
 
 void test_disallow_right_empty_strand(void) {
     TEST_IGNORE();
-    const char strand1[BUFFER_SIZE] = "G";
-    const char strand2[BUFFER_SIZE] = "";
+    alignas(16) const char strand1[BUFFER_SIZE] = "G";
+    alignas(16) const char strand2[BUFFER_SIZE] = "";
     TEST_ASSERT_EQUAL_INT(-1, distance(strand1, strand2));
 }
 
 void test_disallow_empty_second_strand(void) {
     TEST_IGNORE();
-    const char strand1[BUFFER_SIZE] = "G";
-    const char strand2[BUFFER_SIZE] = "";
+    alignas(16) const char strand1[BUFFER_SIZE] = "G";
+    alignas(16) const char strand2[BUFFER_SIZE] = "";
     TEST_ASSERT_EQUAL_INT(-1, distance(strand1, strand2));
 }
 
--- generators/exercises/hamming.py
+++ generators/exercises/hamming.py
@@ -1,6 +1,8 @@
 FUNC_PROTO = """\
 #include "vendor/unity.h"
 
+#include <stdalign.h>
+
 #define BUFFER_SIZE 16
 
 extern int distance(const char strand1[], const char strand2[]);
@@ -10,10 +12,12 @@ extern int distance(const char strand1[], const char strand2[]);
 def gen_func_body(prop, inp, expected):
     strand1 = inp["strand1"]
     strand2 = inp["strand2"]
+    if len(strand1) >= 16 or len(strand2) >= 16:
+        return ""
     if type(expected) is not int:
         expected = -1
     str_list = []
-    str_list.append(f'const char strand1[BUFFER_SIZE] = "{strand1}";')
-    str_list.append(f'const char strand2[BUFFER_SIZE] = "{strand2}";')
+    str_list.append(f'alignas(16) const char strand1[BUFFER_SIZE] = "{strand1}";')
+    str_list.append(f'alignas(16) const char strand2[BUFFER_SIZE] = "{strand2}";')
     str_list.append(f"TEST_ASSERT_EQUAL_INT({expected}, {prop}(strand1, strand2));")
     return "\n".join(str_list)
