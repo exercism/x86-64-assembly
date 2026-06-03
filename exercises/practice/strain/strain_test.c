ba18af541fe8c46b4b13198c8075bd3458044085
oxe-i <oxe.github@gmail.com>
2026-06-03 22:43:20 +0000

    add strain

---
+++ exercises/practice/strain/strain_test.c
@@ -0,0 +1,258 @@
+#include "vendor/unity.h"
+
+#include <stddef.h>
+#include <stdbool.h>
+#include <stdalign.h>
+
+#define ARRAY_SIZE(x) (sizeof(x) / sizeof((x)[0]))
+
+typedef bool (*predicate_t)(const void *);
+
+extern size_t keep(void *elems, size_t elem_size, size_t num_elems, predicate_t fn);
+extern size_t discard(void *elems, size_t elem_size, size_t num_elems, predicate_t fn);
+
+static bool always(const void *x) {
+    (void)x;
+    return true;
+}
+
+static bool never(const void *x) {
+    (void)x;
+    return false;
+}
+
+static bool is_even(const void *x) {
+    const int *y = x;
+    return (*y & 1) == 0;
+}
+
+static bool is_odd(const void *x) {
+    const int *y = x;
+    return (*y & 1) == 1;
+}
+
+static bool starts_with_z(const void *x) {
+    const char *const *y = x;
+    for (int i = 0; *(*y + i); ++i) {
+        if (*(*y + i) == 'z') return true;
+    }
+    return false;
+}
+
+static bool contains_5(const void *x) {
+    const int *y = x;
+    for (int i = 0; i < 3; ++i) {
+        if (y[i] == 5) return true;
+    }
+    return false;
+}
+
+void setUp(void) {
+}
+
+void tearDown(void) {
+}
+
+void test_keep_on_empty_list_returns_empty_list(void) {
+    void *ptr = NULL;
+    size_t elem_size = sizeof(int);
+    size_t num_elems = 0;
+    const size_t size = keep(ptr, elem_size, num_elems, always);
+    TEST_ASSERT_EQUAL_UINT64(0, size);
+}
+
+void test_keeps_everything(void) {
+    TEST_IGNORE();
+    void *ptr = NULL;
+    size_t elem_size = sizeof(int);
+    size_t num_elems = 0;
+    alignas(32) int input[] = {1, 3, 5};
+    ptr = (void *)input;
+    num_elems = ARRAY_SIZE(input);
+    const size_t size = keep(ptr, elem_size, num_elems, always);
+    const int result[] = {1, 3, 5};
+    TEST_ASSERT_EQUAL_UINT64(ARRAY_SIZE(result), size);
+    TEST_ASSERT_EQUAL_INT_ARRAY(result, input, ARRAY_SIZE(result));
+}
+
+void test_keeps_nothing(void) {
+    TEST_IGNORE();
+    void *ptr = NULL;
+    size_t elem_size = sizeof(int);
+    size_t num_elems = 0;
+    alignas(32) int input[] = {1, 3, 5};
+    ptr = (void *)input;
+    num_elems = ARRAY_SIZE(input);
+    const size_t size = keep(ptr, elem_size, num_elems, never);
+    TEST_ASSERT_EQUAL_UINT64(0, size);
+}
+
+void test_keeps_first_and_last(void) {
+    TEST_IGNORE();
+    void *ptr = NULL;
+    size_t elem_size = sizeof(int);
+    size_t num_elems = 0;
+    alignas(32) int input[] = {1, 2, 3};
+    ptr = (void *)input;
+    num_elems = ARRAY_SIZE(input);
+    const size_t size = keep(ptr, elem_size, num_elems, is_odd);
+    const int result[] = {1, 3};
+    TEST_ASSERT_EQUAL_UINT64(ARRAY_SIZE(result), size);
+    TEST_ASSERT_EQUAL_INT_ARRAY(result, input, ARRAY_SIZE(result));
+}
+
+void test_keeps_neither_first_nor_last(void) {
+    TEST_IGNORE();
+    void *ptr = NULL;
+    size_t elem_size = sizeof(int);
+    size_t num_elems = 0;
+    alignas(32) int input[] = {1, 2, 3};
+    ptr = (void *)input;
+    num_elems = ARRAY_SIZE(input);
+    const size_t size = keep(ptr, elem_size, num_elems, is_even);
+    const int result[] = {2};
+    TEST_ASSERT_EQUAL_UINT64(ARRAY_SIZE(result), size);
+    TEST_ASSERT_EQUAL_INT_ARRAY(result, input, ARRAY_SIZE(result));
+}
+
+void test_keeps_strings(void) {
+    TEST_IGNORE();
+    void *ptr = NULL;
+    size_t elem_size = sizeof(const char *);
+    size_t num_elems = 0;
+    alignas(32) const char *input[] = {"apple", "zebra", "banana", "zombies", "cherimoya", "zealot"};
+    ptr = (void *)input;
+    num_elems = ARRAY_SIZE(input);
+    const size_t size = keep(ptr, elem_size, num_elems, starts_with_z);
+    const char *result[] = {"zebra", "zombies", "zealot"};
+    TEST_ASSERT_EQUAL_UINT64(ARRAY_SIZE(result), size);
+    TEST_ASSERT_EQUAL_STRING_ARRAY(result, input, ARRAY_SIZE(result));
+}
+
+void test_keeps_lists(void) {
+    TEST_IGNORE();
+    void *ptr = NULL;
+    size_t elem_size = 3 * sizeof(const int);
+    size_t num_elems = 0;
+    alignas(32) const int input[][3] = {{1, 2, 3}, {5, 5, 5}, {5, 1, 2}, {2, 1, 2}, {1, 5, 2}, {2, 2, 1}, {1, 2, 5}};
+    ptr = (void *)input;
+    num_elems = ARRAY_SIZE(input);
+    const size_t size = keep(ptr, elem_size, num_elems, contains_5);
+    const int result[][3] = {{5, 5, 5}, {5, 1, 2}, {1, 5, 2}, {1, 2, 5}};
+    TEST_ASSERT_EQUAL_UINT64(ARRAY_SIZE(result), size);
+    for (size_t i = 0; i < ARRAY_SIZE(result); ++i) {
+        TEST_ASSERT_EQUAL_INT_ARRAY(result[i], input[i], 3);
+    }
+}
+
+void test_discard_on_empty_list_returns_empty_list(void) {
+    TEST_IGNORE();
+    void *ptr = NULL;
+    size_t elem_size = sizeof(int);
+    size_t num_elems = 0;
+    const size_t size = discard(ptr, elem_size, num_elems, always);
+    TEST_ASSERT_EQUAL_UINT64(0, size);
+}
+
+void test_discards_everything(void) {
+    TEST_IGNORE();
+    void *ptr = NULL;
+    size_t elem_size = sizeof(int);
+    size_t num_elems = 0;
+    alignas(32) int input[] = {1, 3, 5};
+    ptr = (void *)input;
+    num_elems = ARRAY_SIZE(input);
+    const size_t size = discard(ptr, elem_size, num_elems, always);
+    TEST_ASSERT_EQUAL_UINT64(0, size);
+}
+
+void test_discards_nothing(void) {
+    TEST_IGNORE();
+    void *ptr = NULL;
+    size_t elem_size = sizeof(int);
+    size_t num_elems = 0;
+    alignas(32) int input[] = {1, 3, 5};
+    ptr = (void *)input;
+    num_elems = ARRAY_SIZE(input);
+    const size_t size = discard(ptr, elem_size, num_elems, never);
+    const int result[] = {1, 3, 5};
+    TEST_ASSERT_EQUAL_UINT64(ARRAY_SIZE(result), size);
+    TEST_ASSERT_EQUAL_INT_ARRAY(result, input, ARRAY_SIZE(result));
+}
+
+void test_discards_first_and_last(void) {
+    TEST_IGNORE();
+    void *ptr = NULL;
+    size_t elem_size = sizeof(int);
+    size_t num_elems = 0;
+    alignas(32) int input[] = {1, 2, 3};
+    ptr = (void *)input;
+    num_elems = ARRAY_SIZE(input);
+    const size_t size = discard(ptr, elem_size, num_elems, is_odd);
+    const int result[] = {2};
+    TEST_ASSERT_EQUAL_UINT64(ARRAY_SIZE(result), size);
+    TEST_ASSERT_EQUAL_INT_ARRAY(result, input, ARRAY_SIZE(result));
+}
+
+void test_discards_neither_first_nor_last(void) {
+    TEST_IGNORE();
+    void *ptr = NULL;
+    size_t elem_size = sizeof(int);
+    size_t num_elems = 0;
+    alignas(32) int input[] = {1, 2, 3};
+    ptr = (void *)input;
+    num_elems = ARRAY_SIZE(input);
+    const size_t size = discard(ptr, elem_size, num_elems, is_even);
+    const int result[] = {1, 3};
+    TEST_ASSERT_EQUAL_UINT64(ARRAY_SIZE(result), size);
+    TEST_ASSERT_EQUAL_INT_ARRAY(result, input, ARRAY_SIZE(result));
+}
+
+void test_discards_strings(void) {
+    TEST_IGNORE();
+    void *ptr = NULL;
+    size_t elem_size = sizeof(const char *);
+    size_t num_elems = 0;
+    alignas(32) const char *input[] = {"apple", "zebra", "banana", "zombies", "cherimoya", "zealot"};
+    ptr = (void *)input;
+    num_elems = ARRAY_SIZE(input);
+    const size_t size = discard(ptr, elem_size, num_elems, starts_with_z);
+    const char *result[] = {"apple", "banana", "cherimoya"};
+    TEST_ASSERT_EQUAL_UINT64(ARRAY_SIZE(result), size);
+    TEST_ASSERT_EQUAL_STRING_ARRAY(result, input, ARRAY_SIZE(result));
+}
+
+void test_discards_lists(void) {
+    TEST_IGNORE();
+    void *ptr = NULL;
+    size_t elem_size = 3 * sizeof(const int);
+    size_t num_elems = 0;
+    alignas(32) const int input[][3] = {{1, 2, 3}, {5, 5, 5}, {5, 1, 2}, {2, 1, 2}, {1, 5, 2}, {2, 2, 1}, {1, 2, 5}};
+    ptr = (void *)input;
+    num_elems = ARRAY_SIZE(input);
+    const size_t size = discard(ptr, elem_size, num_elems, contains_5);
+    const int result[][3] = {{1, 2, 3}, {2, 1, 2}, {2, 2, 1}};
+    TEST_ASSERT_EQUAL_UINT64(ARRAY_SIZE(result), size);
+    for (size_t i = 0; i < ARRAY_SIZE(result); ++i) {
+        TEST_ASSERT_EQUAL_INT_ARRAY(result[i], input[i], 3);
+    }
+}
+
+int main(void) {
+    UNITY_BEGIN();
+    RUN_TEST(test_keep_on_empty_list_returns_empty_list);
+    RUN_TEST(test_keeps_everything);
+    RUN_TEST(test_keeps_nothing);
+    RUN_TEST(test_keeps_first_and_last);
+    RUN_TEST(test_keeps_neither_first_nor_last);
+    RUN_TEST(test_keeps_strings);
+    RUN_TEST(test_keeps_lists);
+    RUN_TEST(test_discard_on_empty_list_returns_empty_list);
+    RUN_TEST(test_discards_everything);
+    RUN_TEST(test_discards_nothing);
+    RUN_TEST(test_discards_first_and_last);
+    RUN_TEST(test_discards_neither_first_nor_last);
+    RUN_TEST(test_discards_strings);
+    RUN_TEST(test_discards_lists);
+    return UNITY_END();
+}
---
+++ generators/exercises/strain.py
@@ -0,0 +1,126 @@
+FUNC_PROTO = """\
+#include "vendor/unity.h"
+
+#include <stddef.h>
+#include <stdbool.h>
+#include <stdalign.h>
+
+#define ARRAY_SIZE(x) (sizeof(x) / sizeof((x)[0]))
+
+typedef bool(*predicate_t)(const void*);
+
+extern size_t keep(void *elems, size_t elem_size, size_t num_elems, predicate_t fn);
+extern size_t discard(void *elems, size_t elem_size, size_t num_elems, predicate_t fn);
+
+static bool always(const void *x) {
+    (void)x;
+    return true;
+}
+
+static bool never(const void *x) {
+    (void)x;
+    return false;
+}
+
+static bool is_even(const void *x) {
+    const int *y = x;
+    return (*y & 1) == 0;
+}
+
+static bool is_odd(const void *x) {
+    const int *y = x;
+    return (*y & 1) == 1;
+}
+
+static bool starts_with_z(const void *x) {
+    const char * const *y = x;
+    for (int i = 0; *(*y + i); ++i) {
+        if (*(*y + i) == 'z') return true;
+    }
+    return false;
+}
+
+static bool contains_5(const void *x) {
+    const int *y = x;
+    for (int i = 0; i < 3; ++i) {
+        if (y[i] == 5) return true;
+    }
+    return false;
+}
+"""
+
+
+def array_literal(numbers):
+    return str(numbers).replace("[", "{").replace("]", "}").replace("'", '"')
+
+
+def gen_func_body(prop, inp, expected):
+    lst = inp["list"]
+    pred = inp["predicate"]
+    str_list = []
+    if pred == "fn(x) -> starts_with(x, 'z')":
+        str_list.append("void *ptr = NULL;")
+        str_list.append("size_t elem_size = sizeof(const char*);")
+        str_list.append("size_t num_elems = 0;")
+        if lst:
+            str_list.append(f"alignas(32) const char *input[] = {array_literal(lst)};")
+            str_list.append("ptr = (void*)input;")
+            str_list.append("num_elems = ARRAY_SIZE(input);")
+        str_list.append(
+            f"const size_t size = {prop}(ptr, elem_size, num_elems, starts_with_z);"
+        )
+        if expected:
+            str_list.append(f"const char *result[] = {array_literal(expected)};")
+            str_list.append("TEST_ASSERT_EQUAL_UINT64(ARRAY_SIZE(result), size);")
+            str_list.append(
+                "TEST_ASSERT_EQUAL_STRING_ARRAY(result, input, ARRAY_SIZE(result));"
+            )
+        else:
+            str_list.append("TEST_ASSERT_EQUAL_UINT64(0, size);")
+    elif pred == "fn(x) -> contains(x, 5)":
+        str_list.append("void *ptr = NULL;")
+        str_list.append("size_t elem_size = 3 * sizeof(const int);")
+        str_list.append("size_t num_elems = 0;")
+        if lst:
+            str_list.append(f"alignas(32) const int input[][3] = {array_literal(lst)};")
+            str_list.append("ptr = (void*)input;")
+            str_list.append("num_elems = ARRAY_SIZE(input);")
+        str_list.append(
+            f"const size_t size = {prop}(ptr, elem_size, num_elems, contains_5);"
+        )
+        if expected:
+            str_list.append(f"const int result[][3] = {array_literal(expected)};")
+            str_list.append("TEST_ASSERT_EQUAL_UINT64(ARRAY_SIZE(result), size);")
+            str_list.append("for (size_t i = 0; i < ARRAY_SIZE(result); ++i) {")
+            str_list.append("TEST_ASSERT_EQUAL_INT_ARRAY(result[i], input[i], 3);")
+            str_list.append("}")
+        else:
+            str_list.append("TEST_ASSERT_EQUAL_UINT64(0, size);")
+    else:
+        str_list.append("void *ptr = NULL;")
+        str_list.append("size_t elem_size = sizeof(int);")
+        str_list.append("size_t num_elems = 0;")
+        if lst:
+            str_list.append(f"alignas(32) int input[] = {array_literal(lst)};")
+            str_list.append("ptr = (void*)input;")
+            str_list.append("num_elems = ARRAY_SIZE(input);")
+        if pred == "fn(x) -> true":
+            pred = "always"
+        elif pred == "fn(x) -> false":
+            pred = "never"
+        elif pred == "fn(x) -> x % 2 == 1":
+            pred = "is_odd"
+        elif pred == "fn(x) -> x % 2 == 0":
+            pred = "is_even"
+        str_list.append(
+            f"const size_t size = {prop}(ptr, elem_size, num_elems, {pred});"
+        )
+        if expected:
+            str_list.append(f"const int result[] = {array_literal(expected)};")
+            str_list.append("TEST_ASSERT_EQUAL_UINT64(ARRAY_SIZE(result), size);")
+            str_list.append(
+                "TEST_ASSERT_EQUAL_INT_ARRAY(result, input, ARRAY_SIZE(result));"
+            )
+        else:
+            str_list.append("TEST_ASSERT_EQUAL_UINT64(0, size);")
+    return "\n".join(str_list)
