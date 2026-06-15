14b86469847f4be12f549a1e8a84c66a6188abcf
oxe-i <oxe.github@gmail.com>
2026-06-15 22:50:04 +0000

    drop oword to appease macOS

--- exercises/concept/grade-sheet/.meta/exemplar.asm
+++ exercises/concept/grade-sheet/.meta/exemplar.asm
@@ -9,29 +9,29 @@ section .text
 
 global flag_above_threshold
 flag_above_threshold:
-    movaps xmm0, oword [rsi]
-    cmpnleps xmm0, oword [rdx]
-    movaps oword [rdi], xmm0
+    movaps xmm0, [rsi]
+    cmpnleps xmm0, [rdx]
+    movaps [rdi], xmm0
     ret
 
 global flag_perfect
 flag_perfect:
-    movaps xmm0, oword [rsi]
-    cmpeqps xmm0, oword [rdx]
-    movaps oword [rdi], xmm0
+    movaps xmm0, [rsi]
+    cmpeqps xmm0, [rdx]
+    movaps [rdi], xmm0
     ret
 
 global assign_ranks
 assign_ranks:
-    movaps xmm0, oword [rsi]
+    movaps xmm0, [rsi]
     movaps xmm1, xmm0
-    cmpnleps xmm0, oword [rel threshold]
-    movaps xmm2, oword [rel rank_one]
-    blendvps xmm2, oword [rel rank_two]
+    cmpnleps xmm0, [rel threshold]
+    movaps xmm2, [rel rank_one]
+    blendvps xmm2, [rel rank_two]
     movaps xmm0, xmm1
-    cmpeqps xmm0, oword [rdx]
-    blendvps xmm2, oword [rel rank_three]
-    movaps oword [rdi], xmm2
+    cmpeqps xmm0, [rdx]
+    blendvps xmm2, [rel rank_three]
+    movaps [rdi], xmm2
     ret
 
 global count_failures
@@ -40,10 +40,10 @@ count_failures:
     shl rsi, 4
     lea rdi, [rdi + rsi]
     neg rsi
-    movdqa xmm1, oword [rdx]
+    movdqa xmm1, [rdx]
 .loop:
     movdqa xmm0, xmm1
-    pcmpgtd xmm0, oword [rdi + rsi]
+    pcmpgtd xmm0, [rdi + rsi]
     movmskps ecx, xmm0
     popcnt ecx, ecx
     add eax, ecx
@@ -59,7 +59,7 @@ all_passed:
     neg rsi
     pxor xmm0, xmm0
 .loop:
-    movdqa xmm1, oword [rdi + rsi]
+    movdqa xmm1, [rdi + rsi]
     por xmm0, xmm1
     add rsi, 16
     jnz .loop
